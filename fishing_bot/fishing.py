c3a0d8d25878f352c6bf33528eb2eeb7eb27c77c
EscasanN <escalantesanchez3@gmail.com>
2022-09-24 12:56:09 -0600

    The script for the AFK fishing

--- fishing_bot/fishing.py
+++ fishing_bot/fishing.py
@@ -5,18 +5,10 @@ import keyboard
 import win32api, win32con
 
 # functions for the mouse
-# click isn't used
-def click(x,y):
-    win32api.SetCursorPos((x,y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-    time.sleep(0.1)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
-
 def r_click():
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0)
     time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
-
 
 
 print("""****************************************
