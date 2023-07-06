993a4962c8227c4d6d71f5324fc33d0bb89fbf7b
EscasanN <escalantesanchez3@gmail.com>
2023-07-06 16:23:20 -0600

    Made the script lighter

--- fishing_bot/fishing.py
+++ fishing_bot/fishing.py
@@ -1,13 +1,12 @@
-from pyautogui import *
-import pyautogui
-import time 
-import keyboard
+from pyautogui import locateOnScreen
+from time import sleep
+from keyboard import is_pressed
 import win32api, win32con
 
 # functions for the mouse
 def r_click():
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN, 0, 0)
-    time.sleep(0.1)
+    sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP, 0, 0)
 
 
@@ -29,20 +28,20 @@ You can turn off the script by pressing the "q" key on the keyboard.
 print("The script will automatically start when you press enter.\nYou'll have 5 seconds to change the screen to Minecraft after that.")
 input("Press 'Enter' to start>>>")
 
-time.sleep(5)  # for alt-tabbing
+sleep(5)  # for alt-tabbing
 
 # main loop
-while keyboard.is_pressed('q') == False:
+while is_pressed('q') == False:
 
     # check if we are currently NOT fishing
-    if pyautogui.locateOnScreen('Resources/splashing.png', grayscale=True, confidence=0.8) == None:
+    if locateOnScreen('Resources/splashing.png', grayscale=True, confidence=0.8) == None:
         # start fishing
         r_click()
-        time.sleep(2)
+        sleep(2)
 
 
     # check for a bite
-    if pyautogui.locateOnScreen('Resources/fish_splash.png', grayscale=True, confidence=0.8) != None:
-        # a fish bit!
+    if locateOnScreen('Resources/fish_splash.png', grayscale=True, confidence=0.8) != None:
+        # catched a fish!
         r_click()
-        time.sleep(3)
+        sleep(3)
